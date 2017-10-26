9d8a28c1d09a551e502637af0d76f90dd1e4fca9
Joe Kim <josephkim0224@gmail.com>
2017-10-26 15:59:55 -0400

    connect mongodb to app

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
 
 var index = require('./routes/index')
 var api = require('./routes/api')
+
+var dbUrl = 'mongodb://localhost/Social-Bookmark'
+mongoose.connect(dbUrl, function(err, res) {
+  if (err) {
+    console.log('DB Connection failed: ' + err)
+  } else {
+    console.log('DB Connection Success')
+  }
+})
 
 var app = express()
 
