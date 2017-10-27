56a6fd2c571995ea4966ceb66e4fe34d98c97d49
Joe Kim <josephkim0224@gmail.com>
2017-10-27 13:40:11 -0400

    add basic styles to Home

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react'
 class Home extends Component {
   render() {
     return (
-      <div>
-        Home page
+      <div className="row">
+        <div className="col-sm-3" style={{background:'#f9f9f9'}}>
+          Left
+        </div>
+
+        <div className="col-sm-6">
+          Middle
+        </div>
+
+        <div className="col-sm-3" style={{background:'#f9f9f9'}}>
+          Right
+        </div>
       </div>
     )
   }
