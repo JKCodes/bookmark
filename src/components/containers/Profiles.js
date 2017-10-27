2c3853635ba4e7b98c1a6c23bb472d91bb40e06a
Joe Kim <josephkim0224@gmail.com>
2017-10-27 13:47:14 -0400

    create componentDidMount for Profiles container

--- src/components/containers/Profiles.js
+++ src/components/containers/Profiles.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
+import superagent from 'superagent'
 
 class Profiles extends Component {
+  
+  componentDidMount() {
+    superagent
+    .get('/api/profile')
+    .query(null)
+    .set('Accept', 'application/json')
+    .end((err, response) => {
+      if (err) {
+        const msg = err.message || err
+        alert(err)
+
+        return
+      }
+
+      console.log(JSON.stringify(response.body))
+    })
+  }
+
   render() {
     return (
       <div>
