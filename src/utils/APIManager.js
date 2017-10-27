0d100ff604c9a91ddf77a89c2fdd11868fc7fc82
Joe Kim <josephkim0224@gmail.com>
2017-10-27 15:47:09 -0400

    error messages while logging in will now diasplay in an alert

--- src/utils/APIManager.js
+++ src/utils/APIManager.js
@@ -13,6 +13,12 @@ export default {
         return
       }
 
+      const confirmation = response.body.confirmation
+      if (confirmation != 'success') {
+        callback({message: response.body.message}, null)
+        return
+      }
+
       callback(null, response.body)
     })
   },
@@ -25,6 +31,12 @@ export default {
     .end((err, response) => {
       if (err) {
         callback(err, null)
+        return
+      }
+
+      const confirmation = response.body.confirmation
+      if (confirmation != 'success') {
+        callback({message: response.body.message}, null)
         return
       }
 
