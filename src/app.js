861606cae86e88194f4351246d70716597c3d90b
Joe Kim <josephkim0224@gmail.com>
2017-10-27 12:43:26 -0400

    update reacta pp.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Main Page
+      </div>
+    )
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('root'))
