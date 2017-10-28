0d4073d6b8c88b8acca6b67ae145203290a218aa
Joe Kim <josephkim0224@gmail.com>
2017-10-28 11:06:23 -0400

    now loads all Bookmarks on load

--- src/components/containers/Bookmarks.js
+++ src/components/containers/Bookmarks.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
+import actions from '../../actions'
+import { connect } from 'react-redux'
+import { APIManager } from '../../utils'
 
 class Bookmarks extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      bookmarks: []
+    }
+  }
+
+  componentDidMount() {
+    APIManager.get('api/bookmark', null, (err, response) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+
+      this.setState({
+        bookmarks: response.results
+      })
+    })
+  }
 
   render() {
     return (
       <div>
-        Middle Comp
+        <h2>Bookmarks</h2>
+        <ol>
+          {
+            this.state.bookmarks.map((bookmark, i) => {
+              return (
+                <li key={bookmark.id}>{bookmark.title}</li>
+              )
+            })
+          }
+        </ol>
       </div>
     )
   }
